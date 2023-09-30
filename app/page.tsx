baf7cc286e4b42eb171f0c18a5378ba642a4c6b9
Jonathan Nguyen <jonathanguven@gmail.com>
2023-09-30 01:52:10 -0700

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Some content</p>
+      <p>Hello. This is just a super basic note taking app that I am building just to test out Next.js framework. </p>
+      <p>If you navigate to the Notes directory, you can create notes with a title, and they will dynamically appear on the page.</p>
+      <p>This was built with pocketbase from <a href="https://pocketbase.io/" target="_blank">pocketbase.io</a>, download it and sign in to use it with this project.</p>
     </div>
   );
 }
